0b50e50897b68c9bb377f7a07e31c38c06b40967
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-01-19 17:04:02 +0800

    add call to action buttons

--- sbportfolio/src/scenes/Landing.jsx
+++ sbportfolio/src/scenes/Landing.jsx
@@ -56,6 +56,54 @@ const Landing = ({setSelectedPage}) => {
                         </p>
 
                     </motion.div>
+
+                    {/* CALL TO ACTIONS */}
+                    <motion.div
+                        className="flex mt-5 justify-center md:justify-start"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
+                        <AnchorLink
+                            className="bg-gradient-rainblue text-deep-blue rounded-sm py3 px-7 font-semibold
+                                hover:bg-blue hover:text-white transition duration-500"
+                            onClick={() => setSelectedPage("contact")}
+                            href="#contact"
+                        >
+                            Contact Me
+                        </AnchorLink>
+                        <AnchorLink
+                            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                            onClick={() => setSelectedPage("contact")}
+                            href="#contact"
+                        >
+                            <div
+                                className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center 
+                                justify-center font-playfair px-10"
+                            >
+                                Let's Talk
+                            </div>
+                        </AnchorLink>
+                    </motion.div>
+
+                    <motion.div
+                        className="flex mt-5 justify-center md:justify-start"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.4, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
+
+                    </motion.div>
                  </div>
             
         </section>
